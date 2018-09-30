4d4054f216648d280ed9af877e694f7a55ccd14c
U-SYNOPSYS\yuant <yuant@YUANT-7480.internal.synopsys.com>
2018-09-30 15:49:38 +0800

    new for string

--- chapter13/string.cc
+++ chapter13/string.cc
@@ -2,3 +2,15 @@
 #include <iostream>
 
 using namespace std;
+
+void String::alloc_n_copy(char *start, size_type length) {
+  auto newStart = alloc.allocate(length);
+  return {newStart, unitialized_copy(start, start + length, newStart)};
+}
+
+String::String(const char *cStr) {
+  size_type len = sizeof(cStr) / sizeof(cStr[0]);
+  auto newStr = alloc_n_copy(cStr, len);
+  first = newStr.first;
+  last = capacity = newStr.second;
+}
--- chapter13/string.h
+++ chapter13/string.h
@@ -8,23 +8,24 @@ allocator to allocate memory that your String class uses.*/
 class String {
 public:
   String() = default;
-  String(const char* cStr&);
+  String(const char *cStr);
   String(const String str&);
-  String &operator=(const char* cStr&);
+  String &operator=(const char *cStr);
   String &operator=(const String str&);
   ~String();
-  size_type size() const {return end - first;}
+  size_type size() const {return last - first;}
   size_type capacity() const {return capacity - first;}
   std::char *begin() const {return first;}
-  std::char *end() const {return end;}
+  std::char *end() const {return last;}
   std::char front() const {return *first;}
-  std::char back() const {return *end;}
+  std::char back() const {return *last;}
   void insert();
 private:
   std::allocator<std::char> alloc;
   void check_cap() {if (size() == capacity()) reallocate();}
+  pair<std::char*, std::char*> alloc_n_copy(std::char* start, size_type length);
   std::char *first;
-  std::char *end;
+  std::char *last;
   std::char *capacity;
   void free();
   void reallocate();
